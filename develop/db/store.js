164b5be6f029d689a0549961aaac50df01c1f381
JoelP-11 <Jp421513@gmail.com>
2023-05-31 13:10:35 -0700

    updated code, terminal issues almost resolved.

--- develop/db/store.js
+++ develop/db/store.js
@@ -1,7 +1,7 @@
 const util = require('util');
 const fs = require('fs');
 
-const uuidv1 = require('uuid/v1');
+const { v4: uuidv4 } = require('uuid');
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
@@ -33,7 +33,7 @@ class Store {
         if (!title || !text) {
             throw new Error("Note 'title' and 'text' cannot be blank");
         }
-        const newNote = { title, text, id: uuidv1() };
+        const newNote = { title, text, id: uuidv4() };
         return this.getNotes()
         .then((notes) => [...notes, newNote])
         .then((updatedNotes) => this.write(updatedNotes))
